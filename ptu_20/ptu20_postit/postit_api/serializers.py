8e2ffa686263361086cc073e93f22e27162a1211
Polantvic <polantvic@mail.ru>
2024-03-06 07:25:39 +0200

    tema 3

--- ptu_20/ptu20_postit/postit_api/serializers.py
+++ ptu_20/ptu20_postit/postit_api/serializers.py
@@ -19,11 +19,34 @@ class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
 #    comments = serializers.StringRelatedField(many=True, read_only=True)
     comment_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
 
     def get_comment_count(self, obj):
         return models.Comment.objects.filter(post=obj).count()
+    
+    def get_likes_count(self, obj):
+        return models.PostLike.objects.filter(post=obj).count()
 
     class Meta:
         model = models.Post
-        fields = ['id', 'title', 'body', 'created_at', 'updated_at',
-                  'user', 'user_username', 'comment_count', 'comments']
+        fields = ['id', 'title', 'body', 'image', 'created_at',
+                  'updated_at', 'user', 'user_username', 'likes_count',
+                  'comment_count', 'comments']
+
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    post = serializers.ReadOnlyField(source='post.id')
+
+    class Meta:
+        model = models.PostLike
+        fields = ['id', 'user', 'post']
+
+
+class CommentLikeSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    comment = serializers.ReadOnlyField(source='comment.id')
+
+    class Meta:
+        model = models.CommentLike
+        fields = ['id', 'user', 'comment']
